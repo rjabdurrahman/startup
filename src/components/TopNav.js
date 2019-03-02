1bfa160b4cecf91760af6e8cf03e773d5976c1af
rjabdurrahman <md.rahman0296@gmail.com>
2019-03-02 14:54:04 +0600

    Added Css and html header

---
+++ src/components/TopNav.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+class TopNav extends Component {
+    render() {
+        return (
+            <header>
+                <div className="header-nav" style={{ justifyContent: 'space-between' }}>
+                    <div style={{ fontFamily: '"Pacifico", cursive', fontSize: '25px' }}>
+                        <a href="#/">
+                            <img src="img/logo3.png" height={40} />
+                            <img src="img/logo.png" height={30} />
+                        </a>
+                    </div>
+                    <div className="flex top-login">
+                        <input type="text" className="w3-input" placeholder="Username" />
+                        <input type="text" className="w3-input" placeholder="Password" />
+                        <button className="w3-white">Login</button>
+                    </div>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default TopNav;
