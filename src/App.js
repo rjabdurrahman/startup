55b1ebb10a542b24a05c3a949d7d0c38be2d22d1
rjabdurrahman <md.rahman0296@gmail.com>
2019-03-19 23:22:15 +0600

    Removed: Register component and Fixed: Modal Show problem

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './w3.css';
 import Overlay from './components/Overlay';
 import CardArea from './components/CardArea';
 import Footer from './components/Footer';
-import Register from './components/Register';
 
 class App extends Component {
   constructor() {
@@ -29,7 +28,59 @@ class App extends Component {
             <button className="w3-btn main-bg-color" onClick={() => this.showReg()}>Experienced</button>
           </div>
         </Overlay>
-        <Register show={this.state.showRegister} />
+        <div style={this.state.showRegister ? {display: 'block'} : {display : 'none'}} className="w3-modal">
+          <div className="w3-modal-content w3-animate-zoom div-box" style={{ backgroundColor: 'transparent' }}>
+            <div className="w3-light-gray w3-card-4 margin-auto">
+              <div className="w3-container main-bg-color div-title">
+                <span onClick={() => this.setState({ showRegister: false })} className="w3-button w3-display-topright">×</span>
+                <h2>Register</h2>
+              </div>
+              <div className="w3-container">
+                <form id="form1">
+                  <div className="w3-row w3-section">
+                    <div className="w3-col" style={{ width: '150px', padding: '10px' }}>
+                      <label className="w3-medium">Username</label>
+                    </div>
+                    <div className="w3-rest">
+                      <input className="w3-input w3-border" type="text" id="payACCode" required />
+                    </div>
+                  </div>
+                  <div className="w3-row w3-section">
+                    <div className="w3-col" style={{ width: '150px', padding: '10px' }}>
+                      <label className="w3-medium">Email</label>
+                    </div>
+                    <div className="w3-rest">
+                      <input className="w3-input w3-border" type="text" id="payPartyName" required />
+                    </div>
+                  </div>
+                  <div className="w3-row w3-section">
+                    <div className="w3-col" style={{ width: '150px', padding: '10px' }}>
+                      <label className="w3-medium">Password</label>
+                    </div>
+                    <div className="w3-rest">
+                      <input className="w3-input w3-border" type="text" id="payPartyType" required />
+                    </div>
+                  </div>
+                  <div className="w3-row w3-section">
+                    <div className="w3-col" style={{ width: '150px', padding: '10px' }}>
+                      <label className="w3-medium">Re Passowrd</label>
+                    </div>
+                    <div className="w3-rest">
+                      <input className="w3-input w3-border" type="text" id="payProprietor" required />
+                    </div>
+                  </div>
+                  <p>
+                    <input className="w3-check" type="checkbox" />
+                    <label style={{ fontSize: '12px', color: 'indigo' }}>I accept the <a style={{ color: 'blue' }} href="terms">Terms and Conditions</a>.</label></p>
+                  <p>
+                  </p><p className="clearfix">
+                    <button id="submitPayBtn" className="w3-btn main-bg-color div-title" style={{ float: 'right' }}>Submit</button>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
         <CardArea />
         <Footer />
       </div>
