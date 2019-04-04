f62f2167f2b3569e86ed533333dc6b7091cf9c79
rjabdurrahman <md.rahman0296@gmail.com>
2019-04-05 01:12:59 +0600

    Added: submit form and handler

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export class CreatePost extends Component {
     constructor(props) {
@@ -15,14 +16,18 @@ export class CreatePost extends Component {
         }
     }
     inputHandler = (event) => {
-        if(event.target.name === 'skills') {
+        if (event.target.name === 'skills') {
             this.state.post.skills = [];
-            var skills = this.state.post[event.target.name].concat(event.target.value.replace(/,/g,' ').split(' '));
+            var skills = this.state.post[event.target.name].concat(event.target.value.replace(/,/g, ' ').split(' '));
             this.state.post.skills = skills;
         }
         else this.state.post[event.target.name] = event.target.value;
-        this.setState({post : this.state.post});
+        this.setState({ post: this.state.post });
         console.log(this.state);
+    }
+    submitHanler = (event) => {
+        event.preventDefault();
+        console.log('submitted');
     }
     render() {
         return (
@@ -34,32 +39,34 @@ export class CreatePost extends Component {
                         </li>
                     </ul>
                     <div className="post" style={{ margin: '0 15px' }}>
-                        <p>
-                            <select className="w3-select w3-border" name="category" onChange={this.inputHandler}>
-                                <option value="" disabled>Choose your option</option>
-                                <option value="Project Managing">Project Managing</option>
-                                <option value="Requirement Managing">Requirement Managing</option>
-                                <option value="Client Handling">Client Handling</option>
-                                <option value="CRM">CRM</option>
-                                <option value="SRM">SRM</option>
-                            </select>
-                        </p>
-                        <p><label>Post Title</label>
-                            <input className="w3-input w3-border" name="title" type="text" value={this.state.post.title} onChange={this.inputHandler} />
-                        </p>
-                        <p><label>Description</label>
-                            <textarea className="w3-input w3-border" name="description" type="text" value={this.state.post.description} onChange={this.inputHandler}></textarea>
-                        </p>
-                        <p><label>Budget</label>
-                            <input className="w3-input w3-border" name="budget" type="number" value={this.state.post.budget} onChange={this.inputHandler} />
-                        </p>
-                        <p><label>Deadline</label>
-                            <input className="w3-input w3-border" name="deadline" type="date" value={this.state.post.deadline} onChange={this.inputHandler} />
-                        </p>
-                        <p><label>Skills</label>
-                            <input className="w3-input w3-border" name="skills" type="text" value={this.state.post.skills} onChange={this.inputHandler} />
-                        </p>
-                        <p><button className="w3-btn main-bg-color">Submit</button></p>
+                        <form onSubmit={this.submitHanler}>
+                            <p>
+                                <select className="w3-select w3-border" name="category" onChange={this.inputHandler}>
+                                    <option value="" disabled>Choose your option</option>
+                                    <option value="Project Managing">Project Managing</option>
+                                    <option value="Requirement Managing">Requirement Managing</option>
+                                    <option value="Client Handling">Client Handling</option>
+                                    <option value="CRM">CRM</option>
+                                    <option value="SRM">SRM</option>
+                                </select>
+                            </p>
+                            <p><label>Post Title</label>
+                                <input className="w3-input w3-border" name="title" type="text" value={this.state.post.title} onChange={this.inputHandler} />
+                            </p>
+                            <p><label>Description</label>
+                                <textarea className="w3-input w3-border" name="description" type="text" value={this.state.post.description} onChange={this.inputHandler}></textarea>
+                            </p>
+                            <p><label>Budget</label>
+                                <input className="w3-input w3-border" name="budget" type="number" value={this.state.post.budget} onChange={this.inputHandler} />
+                            </p>
+                            <p><label>Deadline</label>
+                                <input className="w3-input w3-border" name="deadline" type="date" value={this.state.post.deadline} onChange={this.inputHandler} />
+                            </p>
+                            <p><label>Skills</label>
+                                <input className="w3-input w3-border" name="skills" type="text" value={this.state.post.skills} onChange={this.inputHandler} />
+                            </p>
+                            <p><button className="w3-btn main-bg-color">Submit</button></p>
+                        </form>
                     </div>
                 </div>
             </div>
